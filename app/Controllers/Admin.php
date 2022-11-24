69659aa821260f589ebdd2604f238124996f4de7
AUDREY TILANOV <tilanovaudrey@gmail.com>
2022-11-24 17:37:09 +0800

    admin controller

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -147,6 +147,7 @@ class Admin extends BaseController
         ->join('tb_kategori_layanan', 'tb_kategori_layanan.id_kategori_layanan = tb_produk.id_kategori_layanan', 'inner')
         ->findAll();
         // dd($produk);
+        // dd($produk);
         // $data = $model->join('tb_produk', 'tb_produk.id_produk = tb_jadwal_produk.id_produk', 'inner')->groupBy('tb_produk.id_produk')->findAll();
         // dd($data);
         $res = [
