3c44bfe1ac04b5763d20806ff891ff6496018f23
Frans701 <franswahyu7@gmail.com>
2022-11-26 05:56:22 +0800

    responsive dashboard user

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -4,27 +4,27 @@
 <?php $session = session() ?>
 
 <!-- Start User Dashboard -->
-<div class="px-[80px] py-[64px]">
-  <div class="grid grid-cols-2 gap-[24px]">
+<div class="px-[24px] xl:px-[80px] py-[64px]">
+  <div class="grid xl:grid-cols-2 gap-[24px]">
     <div class="flex flex-col items-start gap-[32px]">
       <div class="flex flex-col items-start gap-[8px]">
-        <h1 class="font-bold text-5xl mt-[50px]">Detail Akun</h1>
+        <h1 class="font-bold xl:text-5xl xl:mt-[50px] mt-[40px] text-3xl]">Detail Akun</h1>
         <p class="text-base ml-[4px]">
           Di sini kamu bisa mengatur detail akunmu.
         </p>
       </div>
       <div
-        class="p-[40px] border border-zinc-900 w-full flex flex-col items-start gap-[32px] rounded-lg"
+        class="xl:p-[40px] p-[16px] border border-zinc-900 w-full flex flex-col items-start gap-[32px] rounded-lg"
       >
         <div class="flex flex-col items-start gap-[40px]">
-          <div class="flex flex-row gap-[24px] items-center">
+          <div class="xl:flex flex-row gap-[24px] items-center">
             <img
               class="w-[190px] h-[190px] object-cover rounded-lg"
               src="/images/dashboardLogo.png"
               alt=""
             />
             <div class="flex flex-col items-start gap-[8px]">
-              <h1 class="text-2xl font-semibold"><?= $nama_orangtua; ?></h1>
+              <h1 class="xl:text-2xl text-xl font-semibold mt-[8px] xl:mt-[0px]"><?= $nama_orangtua; ?></h1>
               <div class="flex flex-row items-center gap-[16px]">
                 <img class="" src="/icons/maps.svg" alt="" />
                 <p>Kab. Badung, Bali</p>
@@ -32,7 +32,7 @@
             </div>
           </div>
         </div>
-        <div class="flex flex-row items-start w-full items-center">
+        <div class="flex xl:flex-row flex-col items-start w-full items-center gap-[8px]">
           <h3 class="w-full text-base">Nama orang tua :</h3>
           <div
             class="py-[10px] px-[16px] rounded bg-gray-100 w-full rounded-md font-bold text-base"
@@ -49,7 +49,7 @@
             <p>Kab. Badung, Bali</p>
           </div>
         </div> -->
-        <div class="flex flex-row items-start w-full items-center">
+        <div class="flex xl:flex-row flex-col items-start w-full items-center gap-[8px]">
           <h3 class="w-full text-base">Email :</h3>
           <div
             class="py-[10px] px-[16px] rounded bg-gray-100 w-full rounded-md font-bold text-base"
@@ -57,7 +57,7 @@
           <?= $email; ?>
           </div>
         </div>
-        <div class="flex flex-row items-start w-full items-center">
+        <div class="flex xl:flex-row flex-col items-start w-full items-center gap-[8px]">
           <h3 class="w-full text-base">No Whatsapp :</h3>
           <div
             class="py-[10px] px-[16px] rounded bg-gray-100 w-full rounded-md font-bold text-base"
@@ -65,7 +65,7 @@
           <?= $no_whatsapp; ?>
           </div>
         </div>
-        <div class="flex flex-row items-start w-full items-center">
+        <div class="flex xl:flex-row flex-col items-start w-full items-center gap-[8px]">
           <h3 class="w-full text-base">Ubah password :</h3>
           <div
             class="py-[10px] px-[16px] rounded bg-gray-100 w-full rounded-md font-bold text-base"
