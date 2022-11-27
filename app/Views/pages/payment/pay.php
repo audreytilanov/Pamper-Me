493d939ecd1eb225ccbcd2ad0abc8938f665bffb
AUDREY TILANOV <tilanovaudrey@gmail.com>
2022-11-27 14:52:53 +0800

    snap view fix

--- app/Views/pages/payment/pay.php
+++ app/Views/pages/payment/pay.php
@@ -8,7 +8,7 @@
     <script type="text/javascript">
       document.getElementById('pay-button').onclick = function(){
         // SnapToken acquired from previous step
-        snap.pay('<?=$snapToken?>', {
+        snap.pay('<?=$snap?>', {
           // Optional
           onSuccess: function(result){
             /* You may add your own js here, this is just example */ document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
