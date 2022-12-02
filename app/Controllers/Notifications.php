8e577c5caccfc87a6bfe5e6e2959c28c9d6ac8ce
AUDREY TILANOV <tilanovaudrey@gmail.com>
2022-12-02 19:48:20 +0800

    notif

--- app/Controllers/Notifications.php
+++ app/Controllers/Notifications.php
@@ -11,22 +11,50 @@ use Midtrans\Notification;
 class Notifications extends BaseController
 {
    public function index(){
-    // require_once(dirname(__FILE__) . '/Midtrans.php');
-    Config::$isProduction = false;
-    Config::$serverKey = 'SB-Mid-server-3wPaoPW6wHCsFdiUicIvhpVf';
-    $notif = new Notification();
-     
-    $transaction = $notif->transaction_status;
-    $type = $notif->payment_type;
-    $order_id = $notif->order_id;
-    $fraud = $notif->fraud_status;
-
-    $data = new ReservasiModel();
-    if($transaction == "settlement"){
-        $data->update($order_id, [
-            'status_pembayaran'    => $transaction,
-        ]);
-    }
+        // require_once(dirname(__FILE__) . '/Midtrans.php');
+        \Midtrans\Config::$isProduction = false;
+        \Midtrans\Config::$serverKey = 'SB-Mid-server-3wPaoPW6wHCsFdiUicIvhpVf';
+        $notif = new \Midtrans\Notification();
+        
+        $transaction = $notif->transaction_status;
+        $type = $notif->payment_type;
+        $order_id = $notif->order_id;
+        $fraud = $notif->fraud_status;
+        
+        if ($transaction == 'capture') {
+        // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+        if ($type == 'credit_card'){
+            if($fraud == 'challenge'){
+            // TODO set payment status in merchant's database to 'Challenge by FDS'
+            // TODO merchant should decide whether this transaction is authorized or not in MAP
+            echo "Transaction order_id: " . $order_id ." is challenged by FDS";
+            }
+            else {
+            // TODO set payment status in merchant's database to 'Success'
+            echo "Transaction order_id: " . $order_id ." successfully captured using " . $type;
+            }
+            }
+        }
+        else if ($transaction == 'settlement'){
+        // TODO set payment status in merchant's database to 'Settlement'
+        echo "Transaction order_id: " . $order_id ." successfully transfered using " . $type;
+        }
+        else if($transaction == 'pending'){
+        // TODO set payment status in merchant's database to 'Pending'
+        echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
+        }
+        else if ($transaction == 'deny') {
+        // TODO set payment status in merchant's database to 'Denied'
+        echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
+        }
+        else if ($transaction == 'expire') {
+        // TODO set payment status in merchant's database to 'expire'
+        echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is expired.";
+        }
+        else if ($transaction == 'cancel') {
+        // TODO set payment status in merchant's database to 'Denied'
+        echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is canceled.";
+        }
         
    }
 }
