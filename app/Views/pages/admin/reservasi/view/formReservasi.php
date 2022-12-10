e1731d0c0705d1dd4cb679fc6deaa7407609e8f3
AUDREY TILANOV <tilanovaudrey@gmail.com>
2022-12-10 17:12:10 +0800

    width col form reservasi adjust

--- app/Views/pages/admin/reservasi/view/formReservasi.php
+++ app/Views/pages/admin/reservasi/view/formReservasi.php
@@ -24,37 +24,37 @@
             </select>
         </div>
 
-        <div class="form-group form-group-default layananShow col-md-3">
+        <div class="form-group form-group-default layananShow col-md-4">
             <label for="exampleFormControlSelect1">Layanan</label>
             <select class="form-control selectForm id_layanan" autocomplete="off" list="layanan" name="id_layanan" id="layanan" onChange="getKategori()">
             </select>
         </div>
         
-        <div class="form-group form-group-default kategoriShow col-md-3">
+        <div class="form-group form-group-default kategoriShow col-md-4">
             <label for="exampleFormControlSelect1">Kategori Anak</label>
             <select class="form-control selectForm id_kategori" autocomplete="off" list="kategori" name="id_kategori" id="kategori" onChange="getProduk()">
                 
             </select>
         </div>
 
-        <div class="form-group form-group-default produkShow col-md-3">
+        <div class="form-group form-group-default produkShow col-md-4">
             <label for="exampleFormControlSelect1">Produk</label>
             <select class="form-control selectForm id_produk" autocomplete="off" list="produk" name="id_produk" id="produk" onChange="getTanggal()">
 
             </select>
         </div>
 
-        <div class="form-group form-group-default tanggalShow col-md-3">
+        <div class="form-group form-group-default tanggalShow col-md-4">
             <label for="exampleFormControlSelect1">Tanggal</label>
             <input class="form-control" type="date" name="id_tanggal" id="id_tanggal" onChange="getJam()">
         </div>
 
-        <div class="form-group form-group-default jamShow col-md-3">
+        <div class="form-group form-group-default jamShow col-md-4">
             <label for="exampleFormControlSelect1">Jam Reservasi</label>
             <select class="form-control selectForm id_jam" autocomplete="off" list="jam" name="id_jam" id="jam" onChange="">
             </select>
         </div>
-        <div class="form-group form-group-default col-md-12">
+        <div class="form-group form-group-default col-md-4">
             <button type="submit" class="btn btn-success buttonShow" id="submitButton" onclick="submitForm()">Submit</button>
         </div>
     </div>
