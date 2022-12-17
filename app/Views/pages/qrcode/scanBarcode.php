3cbb72d176367e2996e911954df561120cfa685d
Frans701 <franswahyu7@gmail.com>
2022-12-17 17:59:36 +0800

    update tampilan scan barcode

--- app/Views/pages/qrcode/scanBarcode.php
+++ app/Views/pages/qrcode/scanBarcode.php
@@ -3,17 +3,17 @@
 <?= $this->section('content') ?>
 
 <!-- Start User Dashboard -->
-<div class="px-[80px] py-[64px] flex flex-col items-center gap-[40px]">
-    <div class="mt-[64px] flex flex-col items-center gap-[8px]">
-        <h1 class="text-2xl text-pink-500 font-bold">Nama anak :</h1>
+<div class="xl:px-[80px] px-[24px] py-[64px] flex flex-col items-center md:gap-[24px] gap-[16px] bg-pattern">
+    <div class="mt-[64px] flex flex-col items-center gap-[8px] bg-white shadow-md px-[32px] py-[24px] rounded-md">
+        <h1 class="md:text-xl text-base text-pink-500 font-bold">Nama anak :</h1>
         <h1 class="font-bold text-5xl text-pink-500 font-bold"><?= $data['nama_anak'] ?></h1>
-        <h3 class="font-bold text-2xl text-pink-500 font-bold"><?= $data['nama_produk'] ?></h3>
-        <h3 class="font-bold text-2xl text-pink-500 font-bold">Jam :<?= $data['jam'] ?> (<?= $data['durasi'] ?> Menit)</h3>
+        <h3 class="font-bold md:text-xl text-base text-pink-500 font-bold"><?= $data['nama_produk'] ?></h3>
+        <h3 class="font-bold md:text-xl text-base text-pink-500 font-bold">Jam :<?= $data['jam'] ?> (<?= $data['durasi'] ?> Menit)</h3>
     </div>
-    <img class="w-[300px] h-[300px]" src="<?= $dataUri ?>">
-    <div class="flex flex-col items-center gap-[8px]">
-        <h1 class="text-2xl text-pink-500 font-bold">Silahkan tunjukkan barcode ini kepada pegawai</h1>
-        <h1 class="text-2xl text-pink-500 font-bold">Note : Jangan di scan jika layanan belum dilakukan</h1>
+    <img class="w-[300px] xl:h-[300px] object-contain shadow-md p-[16px] rounded-md bg-white" src="<?= $dataUri ?>">
+    <div class="flex flex-col items-center gap-[8px] text-center md:text-start bg-white shadow-md px-[16px] py-[8px] rounded-md">
+        <h1 class="md:text-xl text-base text-pink-500 font-bold">Silahkan tunjukkan barcode ini kepada pegawai</h1>
+        <h1 class="md:text-xl text-base text-pink-500 font-bold">Note : Jangan di scan jika layanan belum dilakukan</h1>
     </div>
 </div>
 <!-- End User Dashboard -->
